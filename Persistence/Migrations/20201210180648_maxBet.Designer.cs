623acf54db228302ecd9f6223bf55c0633615e10
Javier Oviedo <javieroviedo2000@gmail.com>
2020-12-10 12:07:35 -0600

    added migration

--- Persistence/Migrations/20201210180648_maxBet.Designer.cs
+++ Persistence/Migrations/20201210180648_maxBet.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
 namespace Persistence.Migrations
 {
     [DbContext(typeof(LotteryAppContext))]
-    partial class LotteryAppContextModelSnapshot : ModelSnapshot
+    [Migration("20201210180648_maxBet")]
+    partial class maxBet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
